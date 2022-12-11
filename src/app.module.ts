4dea504a911dcb74ed66fc748a9915b316921523
Wanderson Silva <wandersonsilva768@gmail.com>
2022-12-11 03:11:46 -0300

    feat: adição do Mongo / implementação do CRUD de usuários

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://russo:Q5NP1ddXFvjMt0BP@projeto0.1rjdoqd.mongodb.net/test'),
+    UsersModule
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
